adb1eb4aa0f7ee17706c46146fd9cb87596db49c
rococoshka <75371106+rococoshka@users.noreply.github.com>
2021-03-31 13:38:04 +0300

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -10,7 +10,7 @@ upgradeSys_installPack () {
 
 change_ssh_port () {
 	sed -i 's/.Port 22/Port $sshport/' /etc/ssh/sshd_config
-	/etc/init.d/sshd restart
+	/etc/init.d/ssh restart
 }
 
 change_ssh_port
